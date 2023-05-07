a47010c3c374b19fe82bf482821dd580be00475b
marllonwagner <marllonwagner91@gmail.com>
2023-05-07 20:40:14 -0300

    go

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import Race from './Races';
-import Dwarf from './Races/Dwarf';
-import Orc from './Races/Orc';
-import Elf from './Races/Elf';
-import Halfling from './Races/Halfling';
-import Character from './Character';
-import Monster from './Monster';
+// import Race from './Races';
+// import Dwarf from './Races/Dwarf';
+// import Orc from './Races/Orc';
+// import Elf from './Races/Elf';
+// import Halfling from './Races/Halfling';
+// import Character from './Character';
+// import Monster from './Monster';
 
-// const dwarf1 = new Dwarf('xabluzin', 10);
-// const dwarf2 = new Dwarf('xabluzao', 12);
+// // const dwarf1 = new Dwarf('xabluzin', 10);
+// // const dwarf2 = new Dwarf('xabluzao', 12);
 
-const char = new Character('cuzão');
-const mob = new Monster();
-const mob2 = new Monster();
+// const char = new Character('cuzão');
+// const mob = new Monster();
+// const mob2 = new Monster();
 
-mob.receiveDamage(500);
+// mob.receiveDamage(500);
 
-console.log(mob.lifePoints);
+// console.log(mob.lifePoints);
 
-export default Race;
-export { Dwarf, Orc, Elf, Halfling };
+// export default Race;
+// export { Dwarf, Orc, Elf, Halfling };
