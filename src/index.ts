23172b0f237043814d8aa4e05ef8e431ab81abef
marllonwagner <marllonwagner91@gmail.com>
2023-06-12 15:14:47 -0300

    finished

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,24 @@
-// import Race from './Races';
-// import Dwarf from './Races/Dwarf';
-// import Orc from './Races/Orc';
-// import Elf from './Races/Elf';
-// import Halfling from './Races/Halfling';
-// import Character from './Character';
-// import Monster from './Monster';
-// import { PVP } from './Battle';
+import Race from './Races';
+import Dwarf from './Races/Dwarf';
+import Orc from './Races/Orc';
+import Elf from './Races/Elf';
+import Halfling from './Races/Halfling';
+import Character from './Character';
+import { PVP } from './Battle';
 
-// // const dwarf1 = new Dwarf('xabluzin', 10);
-// // const dwarf2 = new Dwarf('xabluzao', 12);
+// const dwarf1 = new Dwarf('xabluzin', 10);
+// const dwarf2 = new Dwarf('xabluzao', 12);
 
-// const char = new Character('cuzão');
-// const char2 = new Character('mongoloide');
-// // const mob = new Monster();
-// // const mob2 = new Monster();
+const char = new Character('player1');
+const char2 = new Character('player2');
+// const mob = new Monster();
+// const mob2 = new Monster();
 
-// const luta = new PVP(char, char2);
+const luta = new PVP(char, char2);
 
-// // mob.receiveDamage(500);
+// mob.receiveDamage(500);
 
-// console.log();
+console.log(luta);
 
-// export default Race;
-// export { Dwarf, Orc, Elf, Halfling };
+export default Race;
+export { Dwarf, Orc, Elf, Halfling };
